f43d071cf4f3c10024ac3ccb0d20eaf4b9222167
talnmizrachi <tal.mizrachi@masterschool.com>
2024-06-06 00:25:42 +0300

    prettifying

--- resources/job_ready_students_onboarding.py
+++ resources/job_ready_students_onboarding.py
@@ -1,13 +1,11 @@
 from global_functions.LoggingGenerator import Logger
+from global_functions.general_functions import write_object_to_db
 from global_functions.job_ready_students_functions import payload_to_job_ready_student_dict
 from flask import request
 from flask.views import MethodView
-from flask_smorest import abort, Blueprint
-from sqlalchemy.exc import SQLAlchemyError
+from flask_smorest import Blueprint
 import os
-
 from models import JobReadyStudentModel
-from db import db
 
 logger = Logger(os.path.basename(__file__).split('.')[0]).get_logger()
 
@@ -23,13 +21,7 @@ class JobReadyStudent(MethodView):
         job_ready_student_dict = payload_to_job_ready_student_dict(data)
         job_ready_student_object = JobReadyStudentModel(**job_ready_student_dict)
         
-        try:
-            db.session.add(job_ready_student_object)
-            db.session.commit()
-        except SQLAlchemyError as e:
-            db.session.rollback()
-            # logger.error(e)
-            abort(500, message=str(e))
+        write_object_to_db(job_ready_student_object)
         
         return job_ready_student_dict['id'], 201
 
@@ -44,13 +36,7 @@ class JobReadyStudent(MethodView):
             job_ready_student_dict = payload_to_job_ready_student_dict(student)
             job_ready_student_object = JobReadyStudentModel(**job_ready_student_dict)
             student_ids.append(job_ready_student_object.id)
-            try:
-                db.session.add(job_ready_student_object)
-                db.session.commit()
-                student_ids.append(job_ready_student_object.id)
-            except SQLAlchemyError as e:
-                db.session.rollback()
-                logger.error(e)
-                abort(500, message=str(e))
+            
+            write_object_to_db(job_ready_student_object)
         
         return {"ids": student_ids}, 201
