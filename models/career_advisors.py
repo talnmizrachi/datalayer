966eefa761cab143109c1d0b884dcc1317b7b547
talnmizrachi <tal.mizrachi@masterschool.com>
2024-06-05 08:45:34 +0300

    Adding versions to packages + removing HS id from CSA

--- models/career_advisors.py
+++ models/career_advisors.py
@@ -6,7 +6,6 @@ class CareerSuccessAdvisor(db.Model):
     __tablename__ = 'career_success_advisors'
     
     id = db.Column(db.String, primary_key=True, default=lambda: str(uuid4().hex))
-    csa_hs_id = db.Column(db.Integer, nullable=False)
     csa_fullname = db.Column(db.String, nullable=False)
     csa_email = db.Column(db.String, nullable=False)
     
