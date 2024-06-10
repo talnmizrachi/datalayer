85d8da6b37ad2b66455e32b11bc5e8597901b47d
talnmizrachi <tal.mizrachi@masterschool.com>
2024-06-10 19:36:06 +0300

    Started working on te receiving end of the calendly link

--- resources/mock_interviews.py
+++ resources/mock_interviews.py
@@ -3,6 +3,7 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint
 from models import MockInterviewModel
+from platforms_webhooks_catchers.zapier.catch_mock_interviewer_details import get_mock_interviewer_data_from_zapier
 import os
 
 
@@ -13,18 +14,19 @@ blueprint = Blueprint('mock_interview_details', __name__, description="A new sta
 @blueprint.route('/mock_interview_setting', methods=['POST'])
 class MockInterviewSetting(MethodView):
     """
-    Right now we have 2 options - from continuing a process or starting a new process.
-    (+ask RnD to make sure that the calendly UTMs are the same)
-    New process UTMS:
-    utm_source=email&utm_medium=company_name&utm_content=job_title&utm_term=new_process
-    Continue process UTMS:
-    utm_source=process_id&utm_medium=type_of_stage&utm_content=stage_of_interview&utm_term=continue
+    A mock interview is created in one of two options:
+    1. Automatic - A process is being initiated/continued -> A stage is being initiated -> a mock is being created
+    1.a. Automatic - A mock interview line will be created automatically with every new stage created
+    2. Manual - When the relevant POC decides that an additional mock is required.
+    1.b. Manual - TBD
     """
+
     def post(self):
         data = request.get_json()
         logger.info(data)
-        new_stage_dict = parse_and_write_to_db_new_stage_in_process(data, source='direct')
-        return new_stage_dict['id'], 201
+        mock_int_dict = get_mock_interviewer_data_from_zapier(data)
+        MockInterviewModel.query.filter_by(process_id=mock_int_dict.get('process_id')  )
+        return mock_int_dict, 201
 
 
 @blueprint.route('/continue_process_typeform', methods=['POST'])
