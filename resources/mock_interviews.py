78ff415c3dabebde50dd28f88997b2ee25003c1f
talnmizrachi <tal.mizrachi@masterschool.com>
2024-06-11 13:39:04 +0300

    Adding mentor to the mock interview

--- resources/mock_interviews.py
+++ resources/mock_interviews.py
@@ -5,6 +5,7 @@ from flask_smorest import Blueprint
 from models import MockInterviewModel
 from platforms_webhooks_catchers.zapier.catch_mock_interviewer_details import get_mock_interviewer_data_from_zapier
 import os
+from db import db
 
 
 logger = Logger(os.path.basename(__file__).split('.')[0]).get_logger()
@@ -25,21 +26,22 @@ class MockInterviewSetting(MethodView):
         data = request.get_json()
         logger.info(data)
         mock_int_dict = get_mock_interviewer_data_from_zapier(data)
-        MockInterviewModel.query.filter_by(process_id=mock_int_dict.get('process_id')  )
+        logger.info(mock_int_dict)
+        this_mock_object = MockInterviewModel.query.filter_by(
+            process_id=mock_int_dict.get('process_id'),
+            stage_in_funnel=mock_int_dict.get('stage_in_funnel'),
+            type_of_stage=mock_int_dict.get('type_of_stage')).first()
+        logger.info(this_mock_object)
+        if this_mock_object is None:
+            abort(404, description="Mock interview not found")
+            
+        this_mock_object.mentor_name = mock_int_dict.get('mentor_name')
+        this_mock_object.mentor_email = mock_int_dict.get('mentor_email')
+        this_mock_object.mock_interview_datetime = mock_int_dict.get('mock_interview_datetime')
+        this_mock_object.additional_details = mock_int_dict.get('additional_details')
+        db.session.commit()
         return mock_int_dict, 201
 
-
-@blueprint.route('/continue_process_typeform', methods=['POST'])
-class ContinueProcessInitiation(MethodView):
-    """
-    This class handles the initiation of a new process via typeform (to be legacy).
-    """
-    def post(self):
-        data = request.get_json()
-        logger.info(data)
-        new_process_dict = parse_and_write_to_db_new_stage_in_process(data, source='typeform')
-        
-        return new_process_dict['id'], 201
 
 
 if __name__ == '__main__':
