b9a8bd81befbbc6bc60b78f4977ca410ace521b4
talnmizrachi <tal.mizrachi@masterschool.com>
2024-06-11 13:37:53 +0300

    Catch new job ready (from hubspot)

---
+++ platforms_webhooks_catchers/hubspot/catch_new_job_ready_student.py
@@ -0,0 +1,21 @@
+def v3_pass_close_deal_webhook_catcher(_data):
+    identifying_dict = dict(company=_data.get("company"),
+                            dealname=_data.get("dealname").replace(chr(127881), ""),
+                            job_title=_data.get("job_title"),
+                            hs_object_id=str(_data.get("hs_object_id")),
+                            hs_is_closed_won=_data.get("hs_is_closed_won"))
+    
+    return identifying_dict
+    
+
+if __name__ == '__main__':
+    data = {'company': 'Cognosante', 'dealname': '🎉Ranga Hande', 'job_title': 'DOL-NCC Junior Reports Analyst',
+            'hs_object_id': 19238354435, 'hs_is_closed_won': False}
+    data_2 = {'company': 'New River Strategies', 'dealname': '🎉Michael DePasquale', 'job_title': 'Data Analyst',
+              'hs_object_id': 58997101, 'hs_is_closed_won': True}
+    
+    a = v3_pass_close_deal_webhook_catcher(data)
+    b = v3_pass_close_deal_webhook_catcher(data_2)
+    
+    print(a)
+    print(b)
