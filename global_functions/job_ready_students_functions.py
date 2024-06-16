223853f03bda4f71700d99703bbec432cca8178e
talnmizrachi <tal.mizrachi@masterschool.com>
2024-06-16 11:11:51 +0300

    Preparing the reader

--- global_functions/job_ready_students_functions.py
+++ global_functions/job_ready_students_functions.py
@@ -2,8 +2,8 @@ from uuid import uuid4
 
 
 def payload_to_job_ready_student_dict(payload):
-    job_ready_student_dict = {'id': str(uuid4().hex),
-                              "student_ms_id": payload.get('masterschool_id'),
+    job_ready_student_dict = {'id': payload.get('id') or str(uuid4().hex),
+                              "student_ms_id": payload.get('student_ms_id'),
                               "hubspot_id": payload.get('hubspot_id'),
                               "domain": payload.get('domain'),
                               'student_firstname': payload.get('student_firstname'),
@@ -12,7 +12,7 @@ def payload_to_job_ready_student_dict(payload):
                               "student_country": payload.get('student_country'),
                               "student_state": payload.get('student_state'),
                               "student_city": payload.get('student_city'),
-                              "student_github_link": payload.get('student_github_link'),
+                              "student_affiliation_project": payload.get('student_affiliation_project'),
                               "student_linkedin_link": payload.get('student_linkedin_link'),
                               "student_cv_link": payload.get('student_cv_link'),
                               "languages_fluency": payload.get('languages_fluency'),
@@ -20,5 +20,7 @@ def payload_to_job_ready_student_dict(payload):
                               "jaq": payload.get('jaq'),
                               "school_master_name": payload.get('school_master_name'),
                               "csa_hs_id": payload.get('csa_hs_id'),
+                              "created_at": payload.get('created_at'),
+                              "smart_matcher_notification": payload.get('smart_matcher_notification'),
                               }
     return job_ready_student_dict
