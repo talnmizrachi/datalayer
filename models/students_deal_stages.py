c3727220eabcf6d7d6d1598b131dcb54d1580498
talnmizrachi <tal.mizrachi@masterschool.com>
2024-06-23 20:12:58 +0300

    removing constraints

--- models/students_deal_stages.py
+++ models/students_deal_stages.py
@@ -6,8 +6,8 @@ class StudentStagesV3(db.Model):
     __tablename__ = 'student_deal_stages'
     
     id = db.Column(db.String, primary_key=True, default=lambda: str(uuid4().hex))
-    student_id = db.Column(db.String, nullable=False, primary_key=True)
-    hubspot_id = db.Column(db.String, nullable=False, unique=True)
+    student_id = db.Column(db.String, nullable=False)
+    hubspot_id = db.Column(db.String, nullable=False)
     stage = db.Column(db.String, nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
     
