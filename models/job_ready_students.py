5feb5b7ae450e9be5403f07f9eb726f991a11dee
talnmizrachi <tal.mizrachi@masterschool.com>
2024-06-23 17:50:47 +0300

    Sanitizing the columns and adding deal owner

--- models/job_ready_students.py
+++ models/job_ready_students.py
@@ -11,20 +11,12 @@ class JobReadyStudentModel(db.Model):
     
     domain = db.Column(db.String, nullable=False)
     
-    student_firstname = db.Column(db.String, nullable=False)
-    student_lastname = db.Column(db.String, nullable=False)
-    student_email = db.Column(db.String, nullable=False)
-    
     student_country = db.Column(db.String, nullable=False)
     student_state = db.Column(db.String, nullable=True)
     student_city = db.Column(db.String, nullable=False)
     
-    student_linkedin_link = db.Column(db.String, nullable=True)
-    student_cv_link = db.Column(db.String, nullable=True)
-    languages_fluency = db.Column(db.String, nullable=True)
-    
     tags = db.Column(db.String, nullable=True)
-    jaq = db.Column(db.String, nullable=False, default='JAQ5')
+    jaq = db.Column(db.String, nullable=False, default='TBD')
     school_master_name = db.Column(db.String, nullable=True)
     
     is_employed = db.Column(db.Boolean, nullable=False, default=False)
@@ -33,7 +25,4 @@ class JobReadyStudentModel(db.Model):
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_timestamp = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
     
-    student_affiliation_project = db.Column(db.String, nullable=True)
-    smart_matcher_notification = db.Column(db.Boolean, nullable=False, default=True)
-
     hubspot_current_deal_stage = db.Column(db.String, nullable=True)
