daf6eca1f63f50b66022b514f5da507c735b996f
talnmizrachi <tal.mizrachi@masterschool.com>
2024-07-04 00:14:51 +0300

    Adding the new cohort adder and changer

--- resources/students/job_ready_cohorts_changes.py
+++ resources/students/job_ready_cohorts_changes.py
@@ -6,19 +6,19 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint
 import os
-from models import JobReadyStudentModel, StudentOwnerChangesModel
+from models import JobReadyStudentModel, StudentCohortChangesModel
 
 logger = Logger(os.path.basename(__file__).split('.')[0]).get_logger()
 
 blueprint = Blueprint("owners' change from hubspot", __name__, description="This_is_a_templated_blueprint")
 
 
-@blueprint.route('/owner_change', methods=['POST'])
+@blueprint.route('/cohort_change', methods=['POST'])
 class JobReadyStudent(MethodView):
     """
-    Workflow URL - https://app.hubspot.com/workflows/9484219/platform/flow/590608921/edit
+    Workflow URL - *****
     
-    Included deals - Getting Interviews, Passing Interviews
+    when the property current_enrollment_cohort changes
     
     incoming payload -
     {
@@ -32,26 +32,26 @@ class JobReadyStudent(MethodView):
         
         this_student = JobReadyStudentModel.query.filter_by(hubspot_id=str(data['hs_object_id'])).first()
         if this_student is None:
-            return f"{this_student} id is missing from the job ready students.", 202
+            return f"{this_student} id is missing from the job ready students for the purpose of cohorts change.", 202
 
         logger.info(f"Got a owner change:\t{data}")
         
-        new_contact_owner = {
-                "student_hubspot_id": str(data['hs_object_id']),
-                "student_hubspot_owner_id": str(data['current_enrollment_school']),
+        new_student_cohort = {
+                "hubspot_id": str(data['hs_object_id']),
+                "student_cohort": str(data['current_enrollment_cohort']),
                       }
         
-        if (new_contact_owner['student_hubspot_owner_id'].isspace() or
-                new_contact_owner['student_hubspot_owner_id'] == "None"):
-            new_contact_owner['student_hubspot_owner_id'] = None
+        if (new_student_cohort['student_cohort'].isspace() or
+                new_student_cohort['student_cohort'] in ("None", "undefined")):
+            new_student_cohort['student_cohort'] = None
             
-        this_student.student_owner = new_contact_owner['student_hubspot_owner_id']
+        this_student.active_cohort = new_student_cohort['hubspot_id']
         this_student.updated_timestamp = datetime.datetime.now()
         
-        student_owner_change = StudentOwnerChangesModel(**new_contact_owner)
-        write_object_to_db(student_owner_change)
+        student_cohort_change = StudentCohortChangesModel(**new_student_cohort)
+        write_object_to_db(student_cohort_change)
         
-        return str(new_contact_owner['student_hubspot_id']), 201
+        return str(new_student_cohort['hubspot_id']), 201
 
 
 if __name__ == '__main__':
