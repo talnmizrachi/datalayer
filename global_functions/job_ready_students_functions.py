feb76287d44af80441923512a7d6f096297dd0c7
talnmizrachi <tal.mizrachi@masterschool.com>
2024-07-18 17:52:00 +0300

    Fixing False BG

--- global_functions/job_ready_students_functions.py
+++ global_functions/job_ready_students_functions.py
@@ -9,58 +9,58 @@ logger = Logger(os.path.basename(__file__).split('.')[0]).get_logger()
 
 
 def check_if_student_has_id_from_legacy_mocks(_payload):
-    return ProcessModel.query.filter_by(hubspot_id=str(_payload.get('hs_object_id'))).first()
+	return ProcessModel.query.filter_by(hubspot_id=str(_payload.get('hs_object_id'))).first()
 
 
 def payload_to_job_ready_student_dict(payload):
+	existing_id = check_if_student_has_id_from_legacy_mocks(payload)
 
-    existing_id = check_if_student_has_id_from_legacy_mocks(payload)
-    
-    job_ready_student_dict = {'id': existing_id.student_id if existing_id else str(uuid4().hex),
-                              "student_ms_id": str(payload.get('student_ms_id')),
-                              "hubspot_id": str(payload.get('hs_object_id')),
-                              "domain": payload.get('domain', "TBD"),
-                              "student_country": payload.get('student_country'),
-                              "student_state": payload.get('student_state'),
-                              "student_city": payload.get('student_city'),
-                              "schoolmaster_id": payload.get('school_master_name'),
-                              "student_owner": payload.get('hubspot_owner_id'),
-                              "created_at": payload.get('created_at'),
-                              "is_employed": payload.get('is_employed', False),
-                              "hubspot_current_deal_stage": payload.get("hubspot_current_deal_stage", "Job Ready"),
-                              "active_cohort": payload.get("current_enrollment_cohort"),
-                              "current_program": "BG" if payload.get("bg___program") is not None else "deferred"
-                              }
-    
-    stage_dict = {
-            "student_id": job_ready_student_dict['id'],
-            "hubspot_id": job_ready_student_dict['hubspot_id'],
-            "stage": job_ready_student_dict['hubspot_current_deal_stage']
-    }
+	job_ready_student_dict = {'id': existing_id.student_id if existing_id else str(uuid4().hex),
+	                          "student_ms_id": str(payload.get('student_ms_id')),
+	                          "hubspot_id": str(payload.get('hs_object_id')),
+	                          "domain": payload.get('domain', "TBD"),
+	                          "student_country": payload.get('student_country'),
+	                          "student_state": payload.get('student_state'),
+	                          "student_city": payload.get('student_city'),
+	                          "schoolmaster_id": payload.get('school_master_name'),
+	                          "student_owner": payload.get('hubspot_owner_id'),
+	                          "created_at": payload.get('created_at'),
+	                          "is_employed": payload.get('is_employed', False),
+	                          "hubspot_current_deal_stage": payload.get("hubspot_current_deal_stage", "Job Ready"),
+	                          "active_cohort": payload.get("current_enrollment_cohort"),
+	                          "current_program": "BG" if payload.get("bg___program", "") != "" else "deferred"
+	                          }
 
-    cohort_dict = {
-        "hubspot_id": job_ready_student_dict['hubspot_id'],
-        "student_cohort": job_ready_student_dict['active_cohort']
+	stage_dict = {
+		"student_id": job_ready_student_dict['id'],
+		"hubspot_id": job_ready_student_dict['hubspot_id'],
+		"stage": job_ready_student_dict['hubspot_current_deal_stage']
+	}
 
-    }
-    return job_ready_student_dict, stage_dict, cohort_dict
+	cohort_dict = {
+		"hubspot_id": job_ready_student_dict['hubspot_id'],
+		"student_cohort": job_ready_student_dict['active_cohort']
+
+	}
+	return job_ready_student_dict, stage_dict, cohort_dict
 
 
 def onboard_function(data):
-    job_ready_student_dict, stage_dict, cohort_dict = payload_to_job_ready_student_dict(data)
-    
-    is_existing = JobReadyStudentModel.query.filter_by(hubspot_id = job_ready_student_dict['hubspot_id']).first()
-    
-    if is_existing is not None:
-        return {"id": job_ready_student_dict['hubspot_id'], "message":"Student is already onboarded"}
-    
-    job_ready_student_object = JobReadyStudentModel(**job_ready_student_dict)
-    student_stage_obj = StudentStagesV3(**stage_dict)
-    student_cohort_obj = StudentCohortChangesModel(**cohort_dict)
+	logger.info(f"Onboarding student - {data}")
+	job_ready_student_dict, stage_dict, cohort_dict = payload_to_job_ready_student_dict(data)
 
-    write_object_to_db(job_ready_student_object)
-    write_object_to_db(student_stage_obj)
-    write_object_to_db(student_cohort_obj)
+	is_existing = JobReadyStudentModel.query.filter_by(hubspot_id=job_ready_student_dict['hubspot_id']).first()
 
-    logger.info(f"Onboarded new student:\t{job_ready_student_dict['id']}")
-    return job_ready_student_dict
+	if is_existing is not None:
+		return {"id": job_ready_student_dict['hubspot_id'], "message": "Student is already onboarded"}
+
+	job_ready_student_object = JobReadyStudentModel(**job_ready_student_dict)
+	student_stage_obj = StudentStagesV3(**stage_dict)
+	student_cohort_obj = StudentCohortChangesModel(**cohort_dict)
+
+	write_object_to_db(job_ready_student_object)
+	write_object_to_db(student_stage_obj)
+	write_object_to_db(student_cohort_obj)
+
+	logger.info(f"Onboarded new student:\t{job_ready_student_dict['id']}")
+	return job_ready_student_dict
--- platforms_webhooks_catchers/hubspot/catch_job_ready_change_in_deal_stage.py
+++ platforms_webhooks_catchers/hubspot/catch_job_ready_change_in_deal_stage.py
@@ -40,7 +40,7 @@ def job_ready_catch_deal_stage(_data):
 	identifying_dict = dict(hubspot_id=str(_data.get("hs_object_id")),
 	                        csa_fullname=_data.get("hubspot_owner_id"),
 	                        hubspot_current_deal_stage=id_to_name[_data.get("dealstage")],
-	                        current_program="BG" if _data.get("bg___program") is not None else "deferred")
+	                        current_program="BG" if _data.get("bg___program","")!="" else "deferred")
 
 	return identifying_dict
 
