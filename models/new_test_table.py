13070141f7fb93494ca9c26db0a30f2edda9298f
talnmizrachi <tal.mizrachi@masterschool.com>
2024-07-21 19:22:10 +0300

    Changes in Alembic

--- models/new_test_table.py
+++ models/new_test_table.py
@@ -8,3 +8,4 @@ class NewTestTableModel(db.Model):
     id = db.Column(db.String, primary_key=True, default=lambda: str(uuid4().hex))
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     some_string = db.Column(db.String, nullable=True)
+    some_float = db.Column(db.Float)
