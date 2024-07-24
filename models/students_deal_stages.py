fa25c372c6b9c614263e837dd3f1f73ae45547f2
talnmizrachi <tal.mizrachi@masterschool.com>
2024-07-24 09:22:42 +0300

    Remove the "student_id" and relying on hubspot id for now in student payments

--- models/students_deal_stages.py
+++ models/students_deal_stages.py
@@ -6,9 +6,10 @@ class StudentStagesV3(db.Model):
     __tablename__ = 'student_deal_stages'
     
     id = db.Column(db.String, primary_key=True, default=lambda: str(uuid4().hex))
-    student_id = db.Column(db.String, nullable=False)
+    student_id = db.Column(db.String, nullable=True)
     hubspot_id = db.Column(db.String, nullable=False)
     stage = db.Column(db.String, nullable=False)
+    # company_if_rel = db.Column(db.String, nullable=True)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
     
 
