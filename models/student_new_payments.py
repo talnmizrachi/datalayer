aed3f52b9abd505aa5ec37d6ffbb561e11d6c79f
talnmizrachi <tal.mizrachi@masterschool.com>
2024-07-24 09:23:03 +0300

    Asserting only one payment per student, and adding the NULL option for the student id

--- models/student_new_payments.py
+++ models/student_new_payments.py
@@ -6,8 +6,8 @@ class StudentNewPaymentModel(db.Model):
 	__tablename__ = 'student_new_payment'
 
 	id = db.Column(db.String, primary_key=True, default=lambda: str(uuid4().hex))
-	hubspot_id = db.Column(db.String, nullable=False)
-	student_id = db.Column(db.String, nullable=False)
+	hubspot_id = db.Column(db.String, nullable=False, unique=True)
+	student_id = db.Column(db.String, nullable=True)
 	type_of_collection = db.Column(db.String, nullable=False)
-	amount = db.Column(db.Float, nullable=True)
+	amount = db.Column(db.Float, nullable=False)
 	created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
