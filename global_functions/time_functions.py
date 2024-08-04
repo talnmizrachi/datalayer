c64ced9d8d138b17e090e2d2acb90c809998dc41
talnmizrachi <tal.mizrachi@masterschool.com>
2024-08-04 11:36:54 +0300

    Fixing the last thursday function

--- global_functions/time_functions.py
+++ global_functions/time_functions.py
@@ -1,11 +1,13 @@
 from datetime import datetime, timedelta
 
 
-def last_thursday_getter(date):
+def last_thursday(date_str):
+    # Convert string to datetime object
+    date = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
+    
     # Calculate the day difference from Thursday (3 in weekday, where Monday is 0)
     weekday = date.weekday()
     if weekday >= 3:
         return date - timedelta(days=(weekday - 3))
     else:
         return date - timedelta(days=(weekday + 4))
-    
