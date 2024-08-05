106bf6864ab443129ee1e1ed965665e8bd8bd1d0
talnmizrachi <tal.mizrachi@masterschool.com>
2024-08-05 18:53:37 +0300

    Getting Owner name from ID (Hubspot Integration)

---
+++ platforms_webhooks_catchers/hubspot/get_owner_name.py
@@ -0,0 +1,27 @@
+import requests
+import os
+from dotenv import load_dotenv
+from global_functions.LoggingGenerator import Logger
+
+logger = Logger(os.path.basename(__file__).split('.')[0]).get_logger()
+load_dotenv()
+
+
+def get_owner_name(user_id):
+	if user_id is None:
+		return "Error - Owner is None"
+	api_key = os.getenv("HUBSPOT_API_KEY")
+	url = f"https://api.hubapi.com/owners/v2/owners/{user_id}"
+	headers = {
+		'Authorization': f'Bearer {api_key}'
+	}
+
+	response = requests.get(url, headers=headers)
+
+	if response.status_code == 200:
+		owner_data = response.json()
+		owner_name = owner_data.get('firstName', '') + ' ' + owner_data.get('lastName', '')
+		return owner_name
+	else:
+		logger.error(f"Error: Unable to fetch owner data (Status code: {response.status_code})")
+		return "Error getting Name from HS"
