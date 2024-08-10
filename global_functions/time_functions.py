630f4c3ac579fd8b87810b7bb9df05ac5d43e3bf
talnmizrachi <tal.mizrachi@masterschool.com>
2024-08-11 00:14:27 +0300

    Creating time functions for easier use

--- global_functions/time_functions.py
+++ global_functions/time_functions.py
@@ -3,6 +3,7 @@ from global_functions.LoggingGenerator import Logger
 from datetime import datetime, timedelta
 logger = Logger(os.path.basename(__file__).split('.')[0]).get_logger()
 
+
 def last_thursday_getter(date_str):
     # Convert string to datetime object
     date = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
@@ -15,3 +16,15 @@ def last_thursday_getter(date_str):
         return date - timedelta(days=(weekday - 3))
     else:
         return date - timedelta(days=(weekday + 4))
+
+    
+def utc_to_date(utc_timestamp):
+    if utc_timestamp is not None:
+        # Convert from milliseconds to seconds if necessary
+        # This checks if the timestamp is far greater than typical Unix epoch time in seconds
+        if utc_timestamp > 1e12:
+            utc_timestamp /= 1000
+        return datetime.utcfromtimestamp(utc_timestamp).date()
+    else:
+        # Return today's date plus 7 days
+        return datetime.utcnow().date() + timedelta(days=7)
