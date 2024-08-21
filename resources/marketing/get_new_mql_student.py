32e39bb0e4f5b4a18dd1e7e584c0002048b83fba
talnmizrachi <tal.mizrachi@masterschool.com>
2024-08-21 18:57:50 +0300

    Adding more hubspot IDs to the Masterschool list Adding the mechanism for updating students

--- resources/marketing/get_new_mql_student.py
+++ resources/marketing/get_new_mql_student.py
@@ -1,5 +1,5 @@
 from global_functions.LoggingGenerator import Logger
-from global_functions.general_functions import write_object_to_db
+from global_functions.general_functions import write_object_to_db, update_objects_in_session
 from global_functions.time_functions import utc_to_date
 from flask import request
 from models import MarketingMqlStudentsModel
@@ -25,10 +25,21 @@ class NewMqlStudent(MethodView):
         
         existing_student = MarketingMqlStudentsModel.query.filter_by(hubspot_id=str(data['hubspot_id'])).first()
         if existing_student is not None:
-            return {"message": 'MQL student already exists'}, 200
+            existing_student.hubspot_created_at = utc_to_date(data['hubspot_created_at'])
+            existing_student.date_mql_entered = utc_to_date(data['date_mql_entered'])
+            existing_student.date_sql_entered = utc_to_date(data['date_sql_entered'])
+            existing_student.date_bg_enrolled_entered = utc_to_date(data['date_bg_enrolled_entered'])
+            existing_student.date_submitted_typeform_entered = utc_to_date(data['date_submitted_typeform_entered'])
+            update_objects_in_session()
+            
+            return {"message": 'MQL student exists - updateing'}, 200
         
         data['hubspot_created_at'] = utc_to_date(data['hubspot_created_at'])
         data['date_mql_entered'] = utc_to_date(data['date_mql_entered'])
+        data['date_sql_entered'] = utc_to_date(data['date_sql_entered'])
+        data['date_bg_enrolled_entered'] = utc_to_date(data['date_bg_enrolled_entered'])
+        data['date_submitted_typeform_entered'] = utc_to_date(data['date_submitted_typeform_entered'])
+        
         new_mql_obj = MarketingMqlStudentsModel(**data)
         write_object_to_db(new_mql_obj)
         
