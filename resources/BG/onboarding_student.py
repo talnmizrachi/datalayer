d84adfe831a12fa39abe549eb49b62d8555ceacb
talnmizrachi <tal.mizrachi@masterschool.com>
2024-10-20 00:18:51 +0300

    Adding updating functions for BG students and reqding from deals

--- resources/BG/onboarding_student.py
+++ resources/BG/onboarding_student.py
@@ -1,10 +1,12 @@
+from datetime import datetime
+
 from global_constants import V2_ENROLLMENT_STATUS_DEAL_STAGE_MAPPING
 from global_functions.LoggingGenerator import Logger
 from global_functions.time_functions import infer_and_transform_date, utc_to_date
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from models import BGStudentModel, BGStudentModel2
+from models import BGStudentModel, BGStudentModel2, BGStudentChangesModel
 import os
 from global_functions.general_functions import write_object_to_db, is_candidate_ms_employee
 from platforms_webhooks_catchers.hubspot.get_owner_name import get_owner_name
@@ -52,15 +54,7 @@ def onboard_bg_function(data):
     return job_ready_student_dict
 
 
-def onboard_bg_function_2(data):
-    logger.info(f"Onboarding BG student - {data}")
-    
-    is_existing = BGStudentModel2.query.filter_by(hubspot_id=str(data['hubspot_id'])).first()
-    
-    if is_existing is not None:
-        logger.info(f"BG Student {data['hubspot_id']} is already onboarded")
-        return {"id": data['hubspot_id'], "message": "BG Student is already onboarded"}
-    
+def parse_hubspot_data_from_enrollment(data):
     job_ready_student_dict = {
             "enrolment_pipeline_stage": V2_ENROLLMENT_STATUS_DEAL_STAGE_MAPPING.get(str(data['hs_pipeline_stage']),
                                                                                     data['hs_pipeline_stage']),
@@ -68,7 +62,7 @@ def onboard_bg_function_2(data):
             "first_name": data.get('firstname'),
             "last_name": data.get('lastname'),
             "domain": data.get('program'),
-            "active_cohort": infer_and_transform_date(data['enrolment_cohort'], '%b-%Y'),
+            "active_cohort": data['cohort'],
             "student_owner": get_owner_name(data.get('hubspot_owner_id')),
             "hs_pipeline": data.get('hs_pipeline'),
             "is_job_ready_text": str(data['is_job_ready']),
@@ -77,14 +71,76 @@ def onboard_bg_function_2(data):
             "plan_duration": data['plan_duration'],
             'enrollment_id': data.get('enrollment_id'),
             'source': data.get('source'),
+            'plan_location': "DE" if data.get('preferred_start_date') != "Enrolled US Student" else "US",
             "object_modified": utc_to_date(data.get('hs_lastmodifieddate'))
-        
     }
     
+    return job_ready_student_dict
+
+
+def parse_hubspot_data_from_deal(data):
+    job_ready_student_dict = {
+            "enrolment_pipeline_stage": "Closed Won",
+            "hubspot_id": str(data['hubspot_id']),
+            "first_name": data.get('firstname'),
+            "last_name": data.get('lastname'),
+            "domain": data.get('program'),
+            "active_cohort": data['enrolment_cohort'],
+            "student_owner": get_owner_name(data.get('hubspot_owner_id')),
+            "hs_pipeline": data.get('hs_pipeline'),
+            "is_job_ready_text": "No",
+            "is_job_ready": False,
+            "email": data['email'],
+            "plan_duration": data['plan_duration'],
+            'enrollment_id': None,
+            'source': "Deal",
+            'plan_location': "DE" if data.get('preferred_start_date') != "Enrolled US Student" else "US",
+            "object_modified": utc_to_date(data.get('hs_lastmodifieddate'))
+    }
+    
+    return job_ready_student_dict
+
+
+def update_object_in_db(existing_student, dict_with_new_info, update_model_logger=BGStudentChangesModel):
+    assert existing_student is not None, "Object to update cannot be None"
+    
+    for key, value in dict_with_new_info.items():
+        # Using getattr and setattr to get and set attributes
+        if getattr(existing_student, key) != value:
+            change_dict = {
+                    "hubspot_id": existing_student.hubspot_id,
+                    "key": key,
+                    "from_value": getattr(existing_student, key),
+                    "to_value": value
+            }
+            logger.info(f"Updating student: {existing_student.hubspot_id} - {key}: {change_dict['from_value']} -> {value}")
+            setattr(existing_student, key, value)
+            change_object = update_model_logger(**change_dict)
+            existing_student.updated_timestamp = datetime.now()
+            
+            write_object_to_db(change_object)
+        
+    return f"Updating {existing_student.hubspot_id}"
+
+
+def onboard_bg_function_from_enrollment(data, source='enrollment'):
+    logger.info(f"Onboarding BG student - {data}")
+    
+    is_existing = BGStudentModel2.query.filter_by(hubspot_id=str(data['hubspot_id'])).first()
+    
+    if source == 'enrollment':
+        job_ready_student_dict = parse_hubspot_data_from_enrollment(data)
+    elif source == 'deal':
+        job_ready_student_dict = parse_hubspot_data_from_deal(data)
+    else:
+        abort(400, description="Invalid source - either 'enrollment' or 'deal'")
+        
+    if is_existing is not None:
+        msg = update_object_in_db(is_existing, job_ready_student_dict)
+        return {"id": data['hubspot_id'], "message": msg}
+    
     job_ready_student_object = BGStudentModel2(**job_ready_student_dict)
-    
     write_object_to_db(job_ready_student_object)
-    
     logger.info(f"Onboarded new student:\t{job_ready_student_dict['hubspot_id']}")
     return job_ready_student_dict
 
@@ -133,12 +189,11 @@ class NewBGStudent(MethodView):
         data_source = data['source']
         
         if data_source == 'enrollment':
-            ...
+            job_ready_student_dict = onboard_bg_function_from_enrollment(data)
         elif data_source == 'deal':
             ...
         else:
             ...
-        job_ready_student_dict = onboard_bg_function_2(data)
         logger.debug(f"Debugging problem - {job_ready_student_dict}")
         return job_ready_student_dict, 201
 
